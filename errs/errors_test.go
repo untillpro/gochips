fc30fd54ed5f6c65f09b517c562ee6a3b6dd8267
Denis Gribanov <d.gribanov@dev.untill.com>
2019-12-05 10:34:54 +0300

    errors improve: errs.Add() and errs.AddE() returns Errors

--- errs/errors_test.go
+++ errs/errors_test.go
@@ -10,13 +10,23 @@ import (
 func TestErrors(t *testing.T) {
 	var errs Errors
 	require.Nil(t, errs)
+
 	errs.Add("error1")
 	require.Equal(t, "error1", errs.Error())
+
 	errs.Add("error2")
 	require.Equal(t, "Multiple errors:\nerror1\nerror2", errs.Error())
+
 	errs.AddE(errors.New("error3"))
 	require.Equal(t, "Multiple errors:\nerror1\nerror2\nerror3", errs.Error())
+
 	errs = append(errs, errors.New("error4"))
 	require.Equal(t, "Multiple errors:\nerror1\nerror2\nerror3\nerror4", errs.Error())
 	require.Equal(t, "error4", errs[3].Error())
+
+	errs = errs.Add("error5")
+	require.Equal(t, "Multiple errors:\nerror1\nerror2\nerror3\nerror4\nerror5", errs.Error())
+
+	errsAsError := error(errs)
+	require.Equal(t, "Multiple errors:\nerror1\nerror2\nerror3\nerror4\nerror5", errsAsError.Error())
 }
