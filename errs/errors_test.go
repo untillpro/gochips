72f3f08768ada45cdaaa5523a308a4f097246b3a
Denis Gribanov <instance6@gmail.com>
2021-06-09 12:38:08 +0300

    errors update

--- errs/errors_test.go
+++ errs/errors_test.go
@@ -3,6 +3,7 @@ package errs
 import (
 	"errors"
 	"fmt"
+	"strconv"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -13,35 +14,41 @@ func TestErrors(t *testing.T) {
 	println(cap(errs))
 	require.Nil(t, errs)
 
+	perr := &errs
+
 	var newErr error
 	errs.AddE(newErr)
 	require.Nil(t, errs)
 
 	errs.Add("error1")
-	fmt.Printf("%p\n", &errs)
-	require.Equal(t, "error1", errs.Error())
+	fmt.Printf("%p\n", perr)
+	require.Equal(t, "error1", perr.Error())
+	require.Equal(t, "error1", Errors(*perr)[0].Error())
 
 	errs.Add("error2")
-	fmt.Printf("%p\n", &errs)
-	require.Equal(t, "Multiple errors:\nerror1\nerror2", errs.Error())
+	fmt.Printf("%p\n", perr)
+	require.Equal(t, "Multiple errors:\nerror1\nerror2", perr.Error())
+	require.Equal(t, "error2", Errors(*perr)[1].Error())
 
 	errs.AddE(errors.New("error3"))
-	fmt.Printf("%p\n", &errs)
-	require.Equal(t, "Multiple errors:\nerror1\nerror2\nerror3", errs.Error())
+	fmt.Printf("%p\n", perr)
+	require.Equal(t, "Multiple errors:\nerror1\nerror2\nerror3", perr.Error())
+	require.Equal(t, "error3", Errors(*perr)[2].Error())
 
 	errs = append(errs, errors.New("error4"))
-	require.Equal(t, "Multiple errors:\nerror1\nerror2\nerror3\nerror4", errs.Error())
-	require.Equal(t, "error4", errs[3].Error())
+	require.Equal(t, "Multiple errors:\nerror1\nerror2\nerror3\nerror4", perr.Error())
+	require.Equal(t, "error4", Errors(*perr)[3].Error())
 
 	errs = errs.Add("error5")
-	require.Equal(t, "Multiple errors:\nerror1\nerror2\nerror3\nerror4\nerror5", errs.Error())
+	require.Equal(t, "Multiple errors:\nerror1\nerror2\nerror3\nerror4\nerror5", perr.Error())
 
-	errsAsError := error(errs)
+	errsAsError := error(perr)
 	require.Equal(t, "Multiple errors:\nerror1\nerror2\nerror3\nerror4\nerror5", errsAsError.Error())
 
 	for i := 0; i < 1200; i++ {
-		errs.Add("test")
+		errs.Add("test" + strconv.Itoa(i))
 	}
-	fmt.Printf("%p\n", &errs)
-	require.Equal(t, 1205, len(errs))
+	fmt.Printf("%p\n", perr)
+	require.Equal(t, 1205, len(*perr))
+	require.Equal(t, "test1199", Errors(*perr)[1204].Error())
 }
